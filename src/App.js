6048bd6b898e2e9aabc9a49648a2f901a8728bd1
gainey.michael <gainey.michael@gmail.com>
2023-09-19 15:26:08 -0700

    feat: fetch function retrieves monsters from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+
+  const fetchMonsters = () => {
+    fetch('https://api.open5e.com/monsters')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setMonsters(data.results)
+    })
+  }
+
+  useEffect(() => {
+    fetchMonsters();
+  }, [])
+
+  console.log(monsters)
+
   return (
     <div className="App">
-      <h1>Roar!</h1>
+      <h1>Monster Search</h1>
+      {monsters.length && (
+          <ul>
+            {monsters.map(monster => (
+              <li key={monster.name}>{monster.name}</li>
+            ))}
+          </ul>
+      )}
     </div>
   );
 }
