3c7829be22ab2f0cc60b95d180fce1c6f03c1add
gainey.michael <gainey.michael@gmail.com>
2023-09-28 12:56:28 -0700

    feat: add home link image and style navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 
   return (
-    <header>
-      <Link to='/monster-manual'>Home</Link>
+    <header className='navContainer'>
+      <Link to='/monster-manual' className='homeLink'>
+        <img src='https://www.aidedd.org/dnd/images/mimic.jpg' 
+        alt="dnd mimic"
+        title="home"
+        className='homeLinkImg' />
+      </Link>
       <h1 className="heading">Monster Search</h1>
     </header>
   )
