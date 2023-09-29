739a4d2596f2d3121c44432e4b839aec79415531
gainey.michael <gainey.michael@gmail.com>
2023-09-29 10:40:49 -0700

    style: MonsterStatsPage sections and img

--- src/Components/MonsterStatsPage.jsx
+++ src/Components/MonsterStatsPage.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
+import { findMonsterImg } from '../services';
 
 const MonsterStatsPage = ({ selectedMonster }) => {
   const [thisMonster, setThisMonster] = useState(null);
+  let monsterFileName = findMonsterImg(selectedMonster);
 
   const fetchOneMonster = (monster) => {
     fetch(`https://www.dnd5eapi.co/api/monsters/${monster.index}`)
@@ -18,35 +20,40 @@ const MonsterStatsPage = ({ selectedMonster }) => {
   }, [selectedMonster]);
 
   return (thisMonster &&
-    <div>
-      <section>
-        <h2>{thisMonster.name}</h2>
-        <p>{thisMonster.size}</p>
-        <p>{thisMonster.type}</p>
-        <p>{thisMonster.alignment}</p>
-        <p>CR:{thisMonster.challenge_rating}</p>
+    <div className='statSheetContainer'>
+      <section className='bioContainer'>
+        <img
+          src={`https://www.aidedd.org/dnd/images/${monsterFileName}.jpg`}
+          onError={(e) => {
+            e.currentTarget.src ='https://icon-library.com/images/no-user-image-icon/no-user-image-icon-3.jpg'}}
+          className='statBlockImg'
+          alt={selectedMonster.name}
+          loading='lazy' />
+        <div className='bioInfo'>
+          <h2 className='monsterNameCard'>{thisMonster.name}</h2>
+          <p>{thisMonster.alignment}</p>
+          <p>{thisMonster.size} {thisMonster.type} - CR: {thisMonster.challenge_rating}</p>
+          <p>AC: {thisMonster.armor_class.value}</p>
+          <p>HP: {thisMonster.hit_points}</p>
+          <p>HP Roll: {thisMonster.hit_points_roll}</p>
+          <p>Hit Dice: {thisMonster.hit_dice}</p>
+          <div className='speedContainer'>
+            {thisMonster.speed.walk &&
+              <p>Walk Speed: {thisMonster.speed.walk}</p>
+            }
+            {thisMonster.speed.fly &&
+              <p>Fly Speed: {thisMonster.speed.fly}</p>
+            }
+            {thisMonster.speed.walk &&
+              <p>Walk Speed: {thisMonster.speed.walk}</p>
+            }
+            {thisMonster.speed.swim &&
+              <p>Swim Speed: {thisMonster.speed.swim}</p>
+            }
+          </div>
+        </div>
       </section>
-      <section>
-        <p>AC:{thisMonster.armor_class.type}</p>
-        <p>HP:{thisMonster.hit_points}</p>
-        <p>Hit Dice:{thisMonster.hit_dice}</p>
-        <p>HP Roll:{thisMonster.hit_points_roll}</p>
-      </section>
-      <section>
-        {thisMonster.speed.walk &&
-          <p>Walk Speed:{thisMonster.speed.walk}</p>
-        }
-        {thisMonster.speed.fly &&
-          <p>Fly Speed:{thisMonster.speed.fly}</p>
-        }
-        {thisMonster.speed.walk &&
-          <p>Walk Speed:{thisMonster.speed.walk}</p>
-        }
-        {thisMonster.speed.swim &&
-          <p>Swim Speed:{thisMonster.speed.swim}</p>
-        }
-      </section>
-      <section>
+      <section className='scoreContainer'>
         <p>Strength:{thisMonster.strength}</p>
         <p>Dexterity:{thisMonster.dexterity}</p>
         <p>Constitution:{thisMonster.constitution}</p>
@@ -54,20 +61,20 @@ const MonsterStatsPage = ({ selectedMonster }) => {
         <p>Wisdom:{thisMonster.wisdom}</p>
         <p>Charisma:{thisMonster.charisma}</p>
       </section>
-      <section>
+      <section className='profContainer'>
         <label>Proficiencies:</label>
         {thisMonster.proficiencies.map(proficiency => (
           <p>{proficiency.proficiency.name}</p>
         )
         )}
       </section>
-      <section>
+      <section className='immResContainer'>
         <label>Vulnerabilities:</label>
         <label>Resistances:</label>
         <label>Damage Immunities:</label>
         <label>Condition Immunities:</label>
       </section>
-      <section>
+      <section className='abilityContainer'>
         {thisMonster.special_abilities &&
           thisMonster.special_abilities.map(ability => (
             <div>
@@ -77,7 +84,7 @@ const MonsterStatsPage = ({ selectedMonster }) => {
           ))
         }
       </section>
-      <section>
+      <section className='actionContainer'>
         {thisMonster.actions &&
           thisMonster.actions.map(action => (
             <div>
@@ -87,7 +94,7 @@ const MonsterStatsPage = ({ selectedMonster }) => {
           ))
         }
       </section>
-      <section>
+      <section className='legActionContainer'>
         {thisMonster.legendary_actions &&
           thisMonster.legendary_actions.map(legAction => (
             <div>
